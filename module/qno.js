969906e5ec0ecd55ebd9213881aa890ddb5ccfba
unknowne1931 <144589550+unknowne1931@users.noreply.github.com>
2024-11-18 22:49:40 +0530

    Add files via upload

---
+++ module/qno.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const QnoSchema = new mongoose.Schema({
+    Time: String,
+    user: String,
+    img : String,
+    Questio : String,
+    qno : String,
+    a : String,
+    b : String,
+    c : String,
+    d : String,
+    Ans : String,
+    lang :String,
+}, { timestamps: true });
+
+module.exports = mongoose.model('Qno_Count', QnoSchema);
